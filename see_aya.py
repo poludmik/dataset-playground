949215d20968c79e27dd9fc25486bbf50c34b107
poludmik <michael.poludin@gmail.com>
2024-08-22 10:10:31 +0200

    Pushing translated 10k dataset instance

--- see_aya.py
+++ see_aya.py
@@ -3,38 +3,36 @@ import random
 from tqdm import tqdm
 
 # Load the dataset from the folder "downloads/aya_hotpotqa_ces":
-# dataset = datasets.load_from_disk('datasets/downloads/aya_all_czech_google_gemma-2-2b-it')
-dataset = datasets.load_from_disk('datasets/downloads/aya_all_czech')
+dataset = datasets.load_from_disk('downloads/aya_all_czech_google_gemma-2-2b-it')
+# dataset = datasets.load_from_disk('datasets/downloads/aya_all_czech')
 
 # create a range of random numbers
-# indices = random.sample(range(len(dataset["train"])), 20)
+indices = random.sample(range(len(dataset["train"])), 10)
+dataset = dataset["train"].select(indices)
 
-# randomly select 10 examples
-# dataset = dataset["train"].select(indices)
-
-# # print the dataset
-# for example in dataset:
-#     print(example["input"])
-#     print("---")
-#     print(example["output"])
-#     print("\n\n")
+# print the dataset
+for example in dataset:
+    print(example["input"])
+    print("---")
+    print(example["output"])
+    print("\n\n")
     
 
-# Find top beginnings of all input texts through counting
-counts = {}
-for example in tqdm(dataset["train"]):
-    first_words = example["inputs"][:30]
-    if first_words in counts:
-        counts[first_words] += 1
-    else:
-        counts[first_words] = 1
+# # Find top beginnings of all input texts through counting
+# counts = {}
+# for example in tqdm(dataset["train"]):
+#     first_words = example["inputs"][:30]
+#     if first_words in counts:
+#         counts[first_words] += 1
+#     else:
+#         counts[first_words] = 1
 
-# Sort the counts
-sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+# # Sort the counts
+# sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
 
-# Print the top 10 beginnings with percentages
-print("Top 10 beginnings of input texts:")
-total = sum(counts.values())
-for i, (beginning, count) in enumerate(sorted_counts[:10]):
-    print(f"{i+1}. {beginning} ({count} examples, {count/total:.2%})")
+# # Print the top 10 beginnings with percentages
+# print("Top 10 beginnings of input texts:")
+# total = sum(counts.values())
+# for i, (beginning, count) in enumerate(sorted_counts[:10]):
+#     print(f"{i+1}. {beginning} ({count} examples, {count/total:.2%})")
 
